e0083946fdf04ee1793606a33d8a4c3c93b08068
Oran <95408708+Connacri@users.noreply.github.com>
2026-07-01 09:33:19 +0100

    Update ledger_provider.dart

--- lib/features/ledger/ledger_provider.dart
+++ lib/features/ledger/ledger_provider.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import '../../core/dag/dag_engine.dart';
 import '../../core/dag/transaction_model.dart';
+import '../../core/p2p/p2p_node.dart';
 
 class LedgerProvider extends ChangeNotifier {
-  final DagEngine dag = DagEngine();
+  final P2PNode node;
+
+  LedgerProvider(this.node) {
+    // Le DagEngine du node notifie déjà `sync.push(tx)` à chaque commit ;
+    // on se branche dessus en plus pour rafraîchir l'UI Ledger en temps réel,
+    // que la tx vienne du réseau ou d'un envoi wallet local.
+    final previousOnCommit = dag.onCommit;
+    dag.onCommit = (tx) {
+      previousOnCommit?.call(tx);
+      notifyListeners();
+    };
+  }
+
+  DagEngine get dag => node.dag;
 
   void addTx(Transaction tx) {
     dag.add(tx);
-    notifyListeners();
   }
 
   List<Transaction> get transactions => dag.all();
