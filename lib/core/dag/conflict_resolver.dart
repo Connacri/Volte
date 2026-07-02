2ab356e1e22bc56b38163c62fc6004b804a6967c
Oran <95408708+Connacri@users.noreply.github.com>
2026-07-02 09:16:04 +0100

    Update conflict_resolver.dart

--- lib/core/dag/conflict_resolver.dart
+++ lib/core/dag/conflict_resolver.dart
@@ -4,8 +4,8 @@ class ConflictResolver {
   Transaction resolve(List<Transaction> candidates) {
     // RULE: highest timestamp + most approvals wins
     candidates.sort((a, b) {
-      final scoreA = a.timestamp + a.approvals.length * 100;
-      final scoreB = b.timestamp + b.approvals.length * 100;
+      final scoreA = a.timestamp + a.parents.length * 100;
+      final scoreB = b.timestamp + b.parents.length * 100;
 
       return scoreB.compareTo(scoreA);
     });
